8b9470f467f2c75059367d060056b4d42b8559d4
Mariano Vargas <mario.vargas29@gmail.com>
2026-09-12 12:26:11 -0700

    Delete gbgg-tracker directory

--- gbgg-tracker/dashboard/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// If deploying to https://<user>.github.io/<repo>/, set base to "/<repo>/".
-// If using a custom domain or user/org root page, set base to "/".
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/",
-});
